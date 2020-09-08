f84c7f0f06dd5cd6941fca07f510af880a81f45e
Takashi Nishikawa <program.takashi@gmail.com>
2020-09-09 07:33:12 +0900

    禁止入力値の設定、入力終了時の画面遷移の追加

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/list/InputMemoFragment.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/list/InputMemoFragment.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.navigation.navOptions
 import jp.co.cyberagent.dojo2020.R
 import jp.co.cyberagent.dojo2020.databinding.FragmentInputMemoBinding
 import jp.co.cyberagent.dojo2020.models.Memo
@@ -45,21 +48,21 @@ class InputMemoFragment : Fragment() {
         val submitButton = view.findViewById<Button>(R.id.btInputMemoSubmit)
         submitButton.setOnClickListener {
             val title = view.findViewById<EditText>(R.id.etInputMemoTitle).text.toString()
-            val hour = view.findViewById<EditText>(R.id.etInputMemoHour).text.toString()
-            val minute = view.findViewById<EditText>(R.id.etInputMemoMinute).text.toString()
+            val hour = view.findViewById<EditText>(R.id.etInputMemoHour).text.toString().toInt()
+            val minute = view.findViewById<EditText>(R.id.etInputMemoMinute).text.toString().toInt()
             val description = view.findViewById<EditText>(R.id.etDescription).text.toString()
-//                if (title != null) {
-////                    makeToast
-//                } else {
-//                    Log.i("test", "null")
-//                }
-            val id = memoListViewModel.editMemo.value!!.id
-            val memo = Memo(id , title, hour.toInt(), minute.toInt(), description)
-            when(id) {
-                0 -> memoListViewModel.saveMemo(memo)
-                else -> memoListViewModel.updateMemo(memo)
+            if (title == null || (hour == 0 && minute == 0) || description == null || minute > 59) {
+                Toast.makeText(context, "入力情報が不適切です", Toast.LENGTH_LONG).show()
+            } else {
+                var id = memoListViewModel.editMemo.value!!.id
+                val memo = Memo(id , title, hour.toInt(), minute.toInt(), description)
+                when(id) {
+                    0 -> memoListViewModel.saveMemo(memo)
+                    else -> memoListViewModel.updateMemo(memo)
+                }
+                memoListViewModel.editMemo.value = Memo(0, "", 0,0,"")
+                findNavController().navigate(R.id.action_input_memo_data_to_navi_memo_list)
             }
-
         }
     }
 }
