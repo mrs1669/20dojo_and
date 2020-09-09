6768c9566c5c659a4650ca9ea128c0bb8008368a
Takumi Muraishi <b1018104@fun.ac.jp>
2020-09-10 05:08:01 +0900

    remove time to text

--- app/src/main/java/jp/co/cyberagent/dojo2020/ui/timer/TimerFragment.kt
+++ app/src/main/java/jp/co/cyberagent/dojo2020/ui/timer/TimerFragment.kt
@@ -51,11 +51,11 @@ class TimerFragment: Fragment(){
         val runnable = object : Runnable {
             override fun run() {
                 if (stopTimerViewFlag == 0){
-                    timeValue++                      // 秒カウンタ+1
-                    timeToText(timeValue)?.let {        // timeToText()で表示データを作り
-                        timerViewModel.timeCountTextViewLiveData    // timeText.textへ代入(表示)
+                    timerViewModel.timeCountTextViewLiveData.observe(viewLifecycleOwner){
+                        timeCountTextView.text = it
                     }
                     handler.postDelayed(this, 1000)  // 1000ｍｓ後に自分にpost
+                    Log.d("as","hello")
                 }
             }
         }
@@ -95,22 +95,6 @@ class TimerFragment: Fragment(){
         super.onStart()
         timerViewModel.tempTextViewLiveData.observe(viewLifecycleOwner){
             tempTextView.text = it
-        }
-        timerViewModel.timeCountTextViewLiveData.observe(viewLifecycleOwner){
-            timeCountTextView.text = it
-        }
-    }
-
-    private fun timeToText(time: Int = 0): String? {
-        return if (time < 0) {
-            null                                    // 時刻が0未満の場合 null
-        } else if (time == 0) {
-            "00:00:00"                            // ０なら
-        } else {
-            val h = time / 3600
-            val m = time % 3600 / 60
-            val s = time % 60
-            "%1$02d:%2$02d:%3$02d".format(h, m, s)  // 表示に整形
         }
     }
 
